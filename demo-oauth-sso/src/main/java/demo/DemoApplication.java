e248c2b692e1379c4fbdb6c7f59463657cb7dbd0
Toshiaki Maki <makingx@gmail.com>
2015-07-31 23:43:34 +0900

    add details endpoint

--- demo-oauth-sso/src/main/java/demo/DemoApplication.java
+++ demo-oauth-sso/src/main/java/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication extends WebSecurityConfigurerAdapter {
 
     @RequestMapping("/")
-    String hello(Authentication authentication) {
+    String hello(OAuth2Authentication authentication) {
         return "Hello " + authentication.getName();
+    }
+
+    @RequestMapping("/details")
+    Object details(OAuth2Authentication authentication) {
+        return authentication.getUserAuthentication();
     }
 
     public static void main(String[] args) {
