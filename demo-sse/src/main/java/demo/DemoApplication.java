83615ce808fd65b21df983485268af9bb63249f1
Toshiaki Maki <makingx@gmail.com>
2015-08-01 01:26:26 +0900

    Polish

--- demo-sse/src/main/java/demo/DemoApplication.java
+++ demo-sse/src/main/java/demo/DemoApplication.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -20,8 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
-@RestController
+@Controller
 public class DemoApplication {
     final BlockingQueue<EmitterAndCount> emitters = new ArrayBlockingQueue<>(10);
     final Logger log = LoggerFactory.getLogger(DemoApplication.class);
